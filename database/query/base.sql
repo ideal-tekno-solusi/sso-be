30d48c3b5cc78b371a091da02e98367273eb58a4
Ryuze <alfianyunianto74@gmail.com>
2025-09-01 11:07:54 +0700

    update config & base query

--- database/query/base.sql
+++ database/query/base.sql
@@ -9,54 +9,6 @@ create schema if not exists sso authorization sso;
 grant all on all tables in schema sso to sso;
 -- end
 
-create table if not exists sso.users (
-	id varchar(50) primary key,
-	name varchar(255) not null,
-	dot timestamp not null,
-	password text not null,
-	insert_date timestamp not null
-);
-
-create table if not exists sso.sessions (
-	id text primary key,
-	user_id varchar(50),
-	client_id varchar(25) not null,
-	code_challenge text not null,
-	code_challenge_method varchar(10) not null,
-	scopes varchar(255),
-	redirect_url text,
-	insert_date timestamp not null
-);
-
-create table if not exists sso.authorization_tokens (
-	id text primary key,
-	session_id text references sso.sessions(id),
-	insert_date timestamp not null
-);
-
-create table if not exists sso.refresh_tokens (
-	id text primary key,
-	user_id varchar(50),
-	insert_date timestamp not null
-)
-
-insert into sso.users (
-	id,
-	name,
-	dot,
-	password,
-	insert_date
-)
-values (
-	'alfian',
-	'alfian',
-	'1997-06-10',
-	'$2a$15$xwGZGcKIURe1kwSt7zTrrOwCCwOfmN9K5SqOu32sJdGj67FJEUfou',
-	now()
-);
-
-=============================================================
-
 create table if not exists sso.clients (
 	id varchar(255) primary key,
 	name varchar(255) not null,
