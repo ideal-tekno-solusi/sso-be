909877bf056b7ba9ceec381713072031c441b139
ryuze <alfianyunianto74@gmail.com>
2025-09-03 12:20:09 +0700

    update

--- internal/sso/handler/user.go
+++ internal/sso/handler/user.go
@@ -8,17 +8,52 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
-	"github.com/sirupsen/logrus"
 )
 
 func (r *RestService) User(ctx echo.Context, params *operation.UserRequest) error {
+	serviceName := "GET User"
+
+	if params.Token == "" {
+		//? get session, will create new if not found
+		sess, err := session.Get("session", ctx)
+		if err != nil {
+			errorMessage := fmt.Sprintf("failed to get session with error: %v", err)
+			utils.ErrorLog(errorMessage, ctx.Path(), serviceName)
+
+			utils.SendProblemDetailJson(ctx, http.StatusInternalServerError, errorMessage, ctx.Path(), uuid.NewString())
+
+			return nil
+		}
+		if sess.IsNew {
+			errorMessage := "failed to process user because token not found in payload and session"
+			utils.ErrorLog(errorMessage, ctx.Path(), serviceName)
+
+			utils.SendProblemDetailJson(ctx, http.StatusUnauthorized, errorMessage, ctx.Path(), uuid.NewString())
+
+			return nil
+		}
+
+		accessToken := sess.Values["access_token"]
+		if accessToken == nil {
+			errorMessage := "access token not found"
+			utils.ErrorLog(errorMessage, ctx.Path(), serviceName)
+
+			utils.SendProblemDetailJson(ctx, http.StatusUnauthorized, errorMessage, ctx.Path(), uuid.NewString())
+
+			return nil
+		}
+
+		params.Token = accessToken.(string)
+	}
+
 	token := strings.Split(params.Token, " ")
 
 	plainText, err := utils.DecryptJwt(token[1])
 	if err != nil {
 		errorMessage := fmt.Sprintf("failed to decrypt jwt with error: %v", err)
-		logrus.Error(errorMessage)
+		utils.ErrorLog(errorMessage, ctx.Path(), serviceName)
 
 		utils.SendProblemDetailJson(ctx, http.StatusUnauthorized, errorMessage, ctx.Path(), uuid.NewString())
 
